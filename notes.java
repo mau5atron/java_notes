dcd249f2c8a0a24dddba77a08d99069c1f113807
mau5atron <gabrielbetancourt96@outlook.com>
2018-06-28 18:38:31 -0400

    inheritance

--- notes.java
+++ notes.java
@@ -468,6 +468,43 @@ protected static String printNumbers(int maxNumbers){
 } 
 
 
+// Since cat extends Animal it gets all of Animals fields and methods 
+	//  this is called inheritance
+
+public class Cat extends Animal{
+	public Cat(){
+		// initializing Cat
+	}
+
+	// Overriding the Animal method
+	public String makeSound(){
+		return "Meow";
+	}
+
+	public static void main(String[] args){
+		Animal fido = new Dog();
+
+		Animal fluffy = new Cat();
+
+		// We can have an array of Animals that contain more specific subclasses
+			// Any overridden methods are used instead because of polymorphism
+
+		Animal[] theAnimals = new Animal[10];
+
+		theAnimals[0] = fido;
+		theAnimals[1] = fluffy;
+
+		System.out.println("Fido says" + theAnimals[0].makeSound());
+		System.out.println("Fluffy says" + theAnimals[1].makeSound());
+
+		// We can also pass subclasses of Animal and they just work
+		speakAnimal(fluffy);
+
+	}
+}
+
+
+
 
 
 
