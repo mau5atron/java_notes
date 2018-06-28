650248076ca5110670316084267d5dd8d8ec0a56
mau5atron <gabrielbetancourt96@outlook.com>
2018-06-28 18:42:52 -0400

    final inheritance

--- notes.java
+++ notes.java
@@ -504,6 +504,28 @@ public class Cat extends Animal{
 }
 
 
+// Since Dog extends Animal it gets all of Animals fields and methods 
+
+public class Dog extends Animal {
+	public Dog(){
+		// initialize dog with the field that were inherited from animal
+	}
+
+	// You can override Animal methods 
+	public String makeSound(){
+		return "Woof";
+	}
+
+	public static void main(String[] args){
+		Dog fido = new Dog();
+
+		fido.setName("Fido");
+
+		System.out.println(fido.getName());
+	}
+}
+
+
 
 
 
